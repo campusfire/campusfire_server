ce1c6960d9547b8151681548b630b56c3bb7e49c
Antoine Rousselot <antoine.rousselotvigier@centrale-marseille.fr>
2018-10-09 18:35:23 +0200

    avec json propre

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Bonjour depuis agastache');
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({"Resultat":"Bonjour depuis agastache"}));
 });
 
 // Listen to the App Engine-specified port, or 8080 otherwise
 const PORT = process.env.PORT || 10402;
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}...`);
+    console.log(`Server listening on port ${PORT}...`);
 });
