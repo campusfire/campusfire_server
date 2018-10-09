931e0bb4c5beff1e5688deecd5152b64003583bf
Antoine Rousselot <antoine.rousselotvigier@centrale-marseille.fr>
2018-10-09 19:06:42 +0200

    first test with post handler

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const express = require('express');
+const bodyParser= require('body-parser');
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post('/handlePost',(req,res) => {
+    var myImage=request.body.imageUrl;
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({"Resultat":myImage}))
+    });
 
 app.get('/', (req, res) => {
     res.setHeader('Content-Type', 'application/json');
     res.send(JSON.stringify({"Resultat":"Bonjour depuis agastache"}));
 });
 
-// Listen to the App Engine-specified port, or 8080 otherwise
+
+
+
 const PORT = process.env.PORT || 10402;
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}...`);
