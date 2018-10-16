e7c32a28a0ec73308c85bb56167fc8550dc105ab
Antoine Rousselot <antoine.rousselotvigier@centrale-marseille.fr>
2018-10-16 17:34:54 +0200

    auto refresh + display of image at /image.jpg

--- index.js
+++ index.js
@@ -1,15 +1,73 @@
 const express = require('express');
 const bodyParser= require('body-parser');
 const app = express();
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
+const reload = require("reload");
+const http = require("http").Server(app);
+var io=require('socket.io')(http);
+const upload = multer({
+    dest: "./uploads"
+    // Pour gérer des limitations sur ce qu'on accepte: https://github.com/expressjs/multer#limits
+  });
+app.set('view engine', 'ejs');
+
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use("/uploads", express.static(__dirname + '/uploads'));
+
 
 app.post('/handlePost',(req,res) => {
-    var myImage=req.body.imageURL;
+    // var myImage=req.body.imageURL;
+    
     res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify({"Resultat":myImage}))
+    res.send(JSON.stringify({"Resultat":myImage}));
+    console.log(myImage);
     });
+//Handle multi part post request
+app.post(
+    "/file-upload",
+    upload.single("Image" /* name attribute of <file> element in your form */),
+    (req, res) => {
+        const tempPath = req.file.path;
+        const targetPath = path.join(__dirname, "./uploads/image.jpg");
+        if (path.extname(req.file.originalname).toLowerCase() === ".jpg") {
+        //Emit socket.io message:
+        //Peut etre plus élégant si j'arrive à faire passer directement l'image en binaire dans le message socket.io
+        io.sockets.emit('refresh-msg', { data: 'whatever'});
+        fs.rename(tempPath, targetPath, err => {
+            if (err) return handleError(err, res);
+    
+            res
+            .status(200)
+            .contentType("text/plain")
+            .end("File uploaded!");
+        });
+        } else {
+        fs.unlink(tempPath, err => {
+            if (err) return handleError(err, res);
+    
+            res
+            .status(403)
+            .contentType("text/plain")
+            .end("Only .jpg files are allowed!");
+        });
+        }
+    }
+);
+//When I receive the socket message:
+io.on('refresh-msg', function (socket) {
+    console.log("oui");
+    console.log(socket);
+});
+
+app.get("/image.jpg", (req, res) => {
+    // res.sendFile(path.join(__dirname, "./uploads/image.jpg"));
+    res.render("handlePost")
+});
+
 
 app.get('/', (req, res) => {
     res.setHeader('Content-Type', 'application/json');
@@ -17,9 +75,10 @@ app.get('/', (req, res) => {
 });
 
 
-
-
+//Define port
 const PORT = process.env.PORT || 10402;
+
+//Run the server using express
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}...`);
 });
