7faf82c8c8444f082a45b72f606d317bf49d56f5
Antoine Rousselot <antoine.rousselotvigier@centrale-marseille.fr>
2018-11-15 18:34:16 +0100

    try with logger

--- index.js
+++ index.js
@@ -6,87 +6,112 @@ const path = require("path");
 const fs = require("fs");
 const reload = require("reload");
 const http = require("http").Server(app);
+const qrcode=require("qrcode-generator");
+
+
 var io=require('socket.io')(http);
 const upload = multer({
     dest: "./uploads"
     // Pour gérer des limitations sur ce qu'on accepte: https://github.com/expressjs/multer#limits
   });
-app.set('view engine', 'ejs');
+
+module.exports = (logger) => {
+
+    app.set('view engine', 'ejs');
 
 
-app.use(bodyParser.urlencoded({limit:'10mb', extended: false }));
-app.use(bodyParser.json());
-app.use("/uploads", express.static(__dirname + '/uploads'));
+    app.use(bodyParser.urlencoded({limit:'10mb', extended: false }));
+    app.use(bodyParser.json());
+    app.use("/uploads", express.static(__dirname + '/uploads'));
 
 
-app.post('/handlePost',(req,res) => {
-    // var myImage=req.body.imageURL;
+    app.post('/handlePost',(req,res) => {
+        // var myImage=req.body.imageURL;
 
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify({"Resultat":myImage}));
-    console.log(myImage);
-    });
-//Handle multi part post request
-app.post(
-    "/file-upload",
-    upload.single("Image" /* name attribute of <file> element in your form */),
-    (req, res) => {
-        const tempPath = req.file.path;
-        const targetPath = path.join(__dirname, "./uploads/image.jpg");
-        if (path.extname(req.file.originalname).toLowerCase() === ".jpg" || path.extname(req.file.originalname).toLowerCase() === ".jpeg" ||path.extname(req.file.originalname).toLowerCase() === ".png")   {
-        //Emit socket.io message:
-        //Peut etre plus élégant si j'arrive à faire passer directement l'image en binaire dans le message socket.io
-        io.sockets.emit('refresh-msg', { data: 'whatever'});
-        fs.rename(tempPath, targetPath, err => {
-            if (err) return handleError(err, res);
-
-            res
-            .status(200)
-            .contentType("text/plain")
-            .end("File uploaded!");
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify({"Resultat":myImage}));
+        console.log(myImage);
         });
-        } else {
-        fs.unlink(tempPath, err => {
-            if (err) return handleError(err, res);
-
-            res
-            .status(403)
-            .contentType("text/plain")
-            .end("Only .jpg files are allowed!");
-        });
+    //Handle multi part post request
+    app.post(
+        "/file-upload",
+        upload.single("Image" /* name attribute of <file> element in your form */),
+        (req, res) => {
+            logger.error("Ma requete:" + req.stringify);
+            const tempPath = req.file.path;
+            const targetPath = path.join(__dirname, "./uploads/image.jpg");
+            try{
+                if (path.extname(req.file.originalname).toLowerCase() === ".jpg" || path.extname(req.file.originalname).toLowerCase() === ".jpeg" || path.extname(req.file.originalname).toLowerCase() === ".png")   {
+                    //Emit socket.io message:
+                    //Peut etre plus élégant si j'arrive à faire passer directement l'image en binaire dans le message socket.io
+                    io.sockets.emit('refresh-msg', { data: 'whatever'});
+                    fs.rename(tempPath, targetPath, err => {
+                        if (err) return handleError(err, res);
+        
+                        res
+                        .status(200)
+                        .contentType("text/plain")
+                        .end("File uploaded!");
+                    });
+                    } else {
+                    fs.unlink(tempPath, err => {
+                        if (err) return handleError(err, res);
+        
+                        res
+                        .status(403)
+                        .contentType("text/plain")
+                        .end("Only .jpg files are allowed!");
+                    });
+                    }
+            }
+            catch(err){
+                logger.error("Error:" + err.stringify);
+            }
         }
-    }
-);
-//When I receive the socket message:
-io.on('refresh-msg', function (socket) {
-    console.log("oui");
-    console.log(socket);
-});
-
-app.get("/image.jpg", (req, res) => {
-    // res.sendFile(path.join(__dirname, "./uploads/image.jpg"));
-    res.render("handlePost")
-});
-
-app.post('/authenticationPlayer1',(req,res) => {
-    var barcodePlayer1=req.body.barcodeSent;
-    res.setHeader('Content-Type', 'application/json');
-    if (barcodePlayer1=="CodeJoueur1Complique") {
-      res.send(JSON.stringify({"Resultat":barcodePlayer1,"AuthStatus":"AuthGranted"}));
-    }
-    else {
-      res.send(JSON.stringify({"Resultat":barcodePlayer1,"AuthStatus":"AuthFailed"}));
-    }
-    console.log('barcode reçu :'+ barcodePlayer1)
+    );
+    //When I receive the socket message:
+    io.on('refresh-msg', function (socket) {
+        console.log("oui");
+        console.log(socket);
     });
 
+    app.get("/image.jpg", (req, res) => {
+        // res.sendFile(path.join(__dirname, "./uploads/image.jpg"));
+        res.render("handlePost")
+    });
 
-app.get('/', (req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify({"Resultat":"Bonjour depuis agastache"}));
-});
+    app.get('/home',(req,res)=>{
+        var typeNumber=4;
+        var errorCorrectionLevel = 'L';
+        var qr=qrcode(typeNumber, errorCorrectionLevel);
+        qr.addData("CodeJoueur1complque");
+        qr.make();
+        document.getElementById('placeHolder').innerHTML = qr.createImgTag();
+        res.render("home")
+
+    });
+
+    app.post('/authenticationPlayer1',(req,res) => {
+        var barcodePlayer1=req.body.barcodeSent;
+        res.setHeader('Content-Type', 'application/json');
+        if (barcodePlayer1=="CodeJoueur1Complique") {
+        res.send(JSON.stringify({"Resultat":barcodePlayer1,"AuthStatus":"AuthGranted"}));
+        }
+        else {
+        res.send(JSON.stringify({"Resultat":barcodePlayer1,"AuthStatus":"AuthFailed"}));
+        }
+        console.log('barcode reçu :'+ barcodePlayer1)
+        });
 
 
+    app.get('/', (req, res) => {
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify({"Resultat":"Bonjour depuis agastache"}));
+    });
+
+    return app;
+
+}
 //Define port
 const PORT = process.env.PORT || 10410;
 
---
+++ logger.js
@@ -0,0 +1,7 @@
+const winston = require('winston');
+const logger = winston.createLogger({
+    format: winston.format.combine(winston.format.timestamp(),
+    winston.format.simple())
+});
+
+logger.add(new winston.transports.File({ filename: 'error.log', level: 'error' }))
