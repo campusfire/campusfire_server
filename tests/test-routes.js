5e60f4126fa15f0315e2afdbd18b13933065ddf6
Francois Carlue <francois.carlue@centrale-marseille.fr>
2019-01-23 10:03:07 +0100

    First routing tests : get functions

--- tests/test-routes.js
+++ tests/test-routes.js
@@ -2,22 +2,18 @@ var expect  = require('chai').expect;
 var request = require('request');
 
 
-it('Main page status', function(done) {
-    request('http://node.oignon.ovh1.ec-m.fr/display' , function(error, response, body) {
-        expect(response.statusCode).to.equal(200);
-        done();
-    });
-});
-
-it('About page content', function(done) {
-    request('http://localhost:8080/about' , function(error, response, body) {
-        expect(response.statusCode).to.equal(404);
-        done();
+describe('Pages status', function() {
+    it('Main page status', function(done) {
+        request('http://localhost:10410' , function(error, response, body) {
+            expect(response.statusCode).to.equal(200);
+            done();
+     });
     });
 
-it('Main page content', function(done) {
-    request('http://localhost:8080' , function(error, response, body) {
-        expect(body).to.equal('Hello World');
-        done();
+    it('Posted text status', function(done) {
+        request('http://localhost:10410/postedText' , function(error, response, body) {
+            expect(response.statusCode).to.equal(200);
+            done();
+        });
     });
 });
