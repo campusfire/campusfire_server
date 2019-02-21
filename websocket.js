52a161caff6a481d7f664291b66cd64a4d19aa47
Francois Carlue <francois.carlue@centrale-marseille.fr>
2019-02-21 18:04:13 +0100

    corrected mistake in websocket.js

--- websocket.js
+++ websocket.js
@@ -28,9 +28,10 @@ module.exports= {
                 context.moveTo(line[0].x * width, line[0].y * height);
                 context.lineTo(line[1].x * width, line[1].y * height);
                 context.stroke();
-            }
+            })
 
-        console.log("eventHandler launched")
-    })
+            console.log("eventHandler launched")
+        })
+    }
 };
 
