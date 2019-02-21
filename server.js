a2f959e8f1a8031cd944a3f3c10510b1a04a5812
Francois Carlue <francois.carlue@centrale-marseille.fr>
2019-02-21 17:32:01 +0100

    created good websocket eventHandler

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 process.env.NODE_ENV = 'prod';
 
 //Define port
-const logger = require('./logger.js')
+const logger = require('./logger.js');
 const config = require('./config');
-const http = require('./index.js')(logger)
-const socket = require('socket.io')
+const http = require('./index.js')(logger);
+const socket = require('socket.io');
+const drawingWebsocket = require('./websocket');
 
-var deploymentTaks=require('./deploymentTasks')
+var deploymentTaks=require('./deploymentTasks');
 //Run the server using express
 var server = http.listen(config.port, () => {
     //Generate 4 qr codes at the deployment of the app
-    deploymentTaks.generateQRCodes()
-    deploymentTaks.generateRegisterForQrCodes()
+    deploymentTaks.generateQRCodes();
+    deploymentTaks.generateRegisterForQrCodes();
+    drawingWebsocket.eventHandler();
     console.log(`Server listening on port ${config.port}...`);
 });
 
--- websocket.js
+++ websocket.js
@@ -1,3 +1,17 @@
+const express = require('express');
+const app = express();
+const http = require("http").Server(app);
+var io=require('socket.io')(http);
+
+// get canvas element and create context
+var canvas  = document.getElementById('drawing');
+var context = canvas.getContext('2d');
+var width   = window.innerWidth;
+var height  = window.innerHeight;
+
+// set canvas to full browser width/height
+canvas.width = width;
+canvas.height = height;
 
 module.exports= {
     // array of all lines drawn
@@ -7,19 +21,16 @@ module.exports= {
     eventHandler: function () {
         io.on('connection', function (socket) {
 
-            // first send the history to the new client
-            for (var i in line_history) {
-                socket.emit('draw_line', {line: line_history[i]});
-            }
-
             // add handler for message type "draw_line".
             socket.on('draw_line', function (data) {
-                // add received line to history
-                line_history.push(data.line);
-                // send line to all clients
-                io.emit('draw_line', {line: data.line});
-            });
-        })
-    }
+                var line = data.line;
+                context.beginPath();
+                context.moveTo(line[0].x * width, line[0].y * height);
+                context.lineTo(line[1].x * width, line[1].y * height);
+                context.stroke();
+            }
+
+        console.log("eventHandler launched")
+    })
 };
 
