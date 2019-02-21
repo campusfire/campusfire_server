f46bf9612a85f3bbd56fa50407467453e57a2551
Francois Carlue <francois.carlue@centrale-marseille.fr>
2019-02-21 21:53:35 +0100

    changed treatment of json in drawningClient

--- client/drawingClient.js
+++ client/drawingClient.js
@@ -13,14 +13,12 @@ document.addEventListener("DOMContentLoaded", function() {
     // draw line received from server
     socket.on('draw_line', function (data) {
         console.log("client received draw_line");
-        var pos_prev = JSON.parse(data.line).pos_prev;
-        var pos = JSON.parse(data.line).pos;
         console.log("data.line", data.line);
-        console.log("pos_prev",pos_prev);
-        console.log("x de pos_prev",JSON.parse(pos_prev).x);
+        console.log("pos_prev",data.line.pos_prev);
+        console.log("x de pos_prev",data.line.pos_prev.x);
         context.beginPath();
-        context.moveTo(JSON.parse(pos_prev).x * width, JSON.parse(pos_prev).y * height);
-        context.lineTo(JSON.parse(pos).x * width, JSON.parse(pos).y * height);
+        context.moveTo(data.line.pos_prev.x * width, data.line.pos_prev.y * height);
+        context.lineTo(data.line.pos.x * width, data.line.pos.y * height);
         context.stroke();
     });
 });
