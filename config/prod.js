d03307842e662d3c65b5acc1a5f77f28e7cb69ac
Francois Carlue <francois.carlue@centrale-marseille.fr>
2019-02-22 08:23:05 +0100

    changed port back to 10410 for oignon

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,6 @@
 let config = {
     env: 'prod',
-    port: 10424,
+    port: 10410,
     baseUrl: 'localhost',
     textFile: './postedText.txt',
     qrRegister: './qrCodes/QRCodeRegister.json',
--- websocket.js
+++ websocket.js
@@ -10,7 +10,7 @@ module.exports= {
     // event-handler for new incoming connections
     eventHandler: function () {
         io.on('connection', function (socket) {
-            console.log("incoming connection");
+            console.log("incoming socket connection");
 
             // first send the history to the new client
             for (var i in line_history) {
